1d7536a07fcfcae0da1bae4b05cc050f474382d1
py.chih <poyuanchih@gapp.nthu.edu.tw>
2021-01-09 13:15:09 -0800

    day1: find pivot index

--- Data_structure_intro/Array_and_String/Intro_to_array/Find_Pivot_Index/Find_Pivot_Index.cpp
+++ Data_structure_intro/Array_and_String/Intro_to_array/Find_Pivot_Index/Find_Pivot_Index.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int sum_val = 0;
-        for(int val : nums){
-            sum_val += val;
-        }
-        int sz = nums.size();
-        int pre = 0;
-        for(int i = 0; i < sz; ++i){
-            if(pre*2 + nums[i] == sum_val){
-                return i;
-            }else{
-                pre += nums[i];
-            }
+        auto run_iter=nums.begin();
+        if (run_iter==nums.end()){return -1;}
+        run_iter+=1;
+        int right_sum = accumulate(run_iter, nums.end(), 0);
+        if (right_sum==0){return 0;}
+        int left_sum=0;
+        if (nums.size()<3){return -1;}
+        for(; run_iter!=nums.end(); ++run_iter){
+            left_sum+=*(run_iter-1);
+            right_sum -= *run_iter;
+            if (right_sum==left_sum){
+                return (run_iter - nums.begin());
+            } 
         }
         return -1;
     }
--- Data_structure_intro/Array_and_String/Intro_to_array/Find_Pivot_Index/README.md
+++ Data_structure_intro/Array_and_String/Intro_to_array/Find_Pivot_Index/README.md
@@ -1,5 +1,15 @@
 # Problem Description
 - [Find_Pivot_index](https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1144/)
 
-# 
-# 
+- [x] 問題: int最大容量多是多少?
+- ans : 
+    - https://docs.microsoft.com/zh-tw/cpp/c-language/cpp-integer-limits?view=msvc-160
+    - https://programming.im.ncnu.edu.tw/Chapter5.htm
+    - https://blog.xuite.net/tsai.oktomy/program/64004343-C%2B%2B+%E7%9A%84%E5%9F%BA%E6%9C%AC%E8%B3%87%E6%96%99%E5%9E%8B%E6%85%8B
+- 心得:
+    - 要先把corner case 想清楚
+        - 空vector
+        - vector.size ==1
+        - vector.size ==2
+        - 啥時會return index 0
+    - 把range for的sum vector改成了accumulate
